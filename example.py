f4172519f31cc82f7548833f1d42fb8608cb1e9a
rylans <rylans@gmail.com>
2016-10-27 11:14:21 -0700

    a serialization function

--- example.py
+++ example.py
@@ -6,5 +6,15 @@ from pyoptimus import pyoptimus
 # instantiate an instance
 instance = pyoptimus()
 
-# get the most recent build result
-print instance.build_results()[0]
+# get recent build results
+res = instance.build_results()
+
+# print a build result
+print res[0]
+
+# print number of build results retrieved
+print "Total build results: ", len(res)
+
+# grab build results for a certain connector
+sp_results = instance.build_results('sharepoint')
+print "Sharepoint build results: ", len(sp_results), sp_results[0]
--- pyoptimus.py
+++ pyoptimus.py
@@ -10,15 +10,32 @@ class pyoptimus:
         self.OPTIMUS_URL = 'http://optimus.van.tasktop.com'
         self.GET_RESULTS = '/build_results'
 
-    def build_results(self):
+    def build_results(self, connector=None):
         url = self.OPTIMUS_URL + self.GET_RESULTS
         response = urllib2.urlopen(url)
-        data = json.load(response)
+        if connector:
+            data = [c for c in json.load(response) if connector in c['connector_kind']]
+        else:
+            data = json.load(response)
         return data
+
+    def serialize_build(self, build):
+        connector_id = int(build['connector_id'])
+        pass_count = int(build['pass_count'])
+        fail_count = int(build['fail_count'])
+        skip_count = int(build['skip_count'])
+        duration = int(build['duration'])
+
+        count = float(pass_count + fail_count + skip_count)
+
+        return [float(connector_id), pass_count/count, fail_count/count, skip_count/count, float(duration)]
+
 
 def main():
     instance = pyoptimus()
-    print instance.build_results()
+    results = instance.build_results()
+    print results[0]
+    print instance.serialize_build(results[0])
 
 if __name__ == '__main__':
     main()
