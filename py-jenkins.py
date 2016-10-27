1903915e456a978e0931a8f03ebf407581381cb0
rylans <rylans@gmail.com>
2016-10-27 15:42:46 -0700

    refactoring

--- py-jenkins.py
+++ py-jenkins.py
@@ -1,24 +1,29 @@
 '''python jenkins
-how to trigger a build'''
+build trigger api'''
 
 import requests
 from requests.auth import HTTPBasicAuth
-import sys
 import getpass
 
-username = raw_input('Username: ')
-password = getpass.getpass('Password: ')
+class pyjenkins:
+    trigger_url_start = 'https://ci-comp.tasktop.com/job/'
+    trigger_url_end = '/build?delay=0sec'
 
-trigger_url_start = 'https://ci-comp.tasktop.com/job/'
-trigger_url_end = '/build?delay=0sec'
+    def __init__(self):
+        self.start = pyjenkins.trigger_url_start
+        self.end = pyjenkins.trigger_url_end
 
-connector_name = 'connector-atlassian-jira'
+    def trigger(self, connector_job_name, username, password):
+        print "Triggering connector: " + connector_job_name
+        full_url = self.start + connector_job_name + self.end
+        print full_url
+        resp = requests.post(full_url, auth=HTTPBasicAuth(username, password))
+        return resp
 
-def trigger_build(con, username, password):
-    print "Connector Name: " + con
-    full_url = trigger_url_start + con + trigger_url_end
-    print full_url
-    resp = requests.post(full_url , auth=HTTPBasicAuth(username, password))
-    print resp
+if __name__ == '__main__':
+    username = raw_input('Username: ')
+    password = getpass.getpass('Password: ')
+    connector_name = raw_input('Connector job name to trigger: ')
 
-trigger_build(connector_name, username, password)
+    jenkins = pyjenkins()
+    print jenkins.trigger(connector_name, username, password)
