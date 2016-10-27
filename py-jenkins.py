cecaff4ba7166720085b0786fdef6d6d5537a86c
rylans <rylans@gmail.com>
2016-10-27 15:53:36 -0700

    ask for credentials on instantiation

--- py-jenkins.py
+++ py-jenkins.py
@@ -9,15 +9,17 @@ class pyjenkins:
     trigger_url_start = 'https://ci-comp.tasktop.com/job/'
     trigger_url_end = '/build?delay=0sec'
 
-    def __init__(self):
+    def __init__(self, username, password):
         self.start = pyjenkins.trigger_url_start
         self.end = pyjenkins.trigger_url_end
+        self.username = username
+        self.password = password
 
-    def trigger(self, connector_job_name, username, password):
+    def trigger(self, connector_job_name):
         print "Triggering connector: " + connector_job_name
         full_url = self.start + connector_job_name + self.end
         print full_url
-        resp = requests.post(full_url, auth=HTTPBasicAuth(username, password))
+        resp = requests.post(full_url, auth=HTTPBasicAuth(self.username, self.password))
         return resp
 
 if __name__ == '__main__':
@@ -25,5 +27,5 @@ if __name__ == '__main__':
     password = getpass.getpass('Password: ')
     connector_name = raw_input('Connector job name to trigger: ')
 
-    jenkins = pyjenkins()
-    print jenkins.trigger(connector_name, username, password)
+    jenkins = pyjenkins(username, password)
+    print jenkins.trigger(connector_name)
