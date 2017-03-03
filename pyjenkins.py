531293e59f1fcf2189e4123a9bb9ff2966526940
rylans <rylans@gmail.com>
2017-03-03 11:31:20 -0800

    check login credentials

--- pyjenkins.py
+++ pyjenkins.py
@@ -3,9 +3,11 @@ build trigger api'''
 
 import requests
 from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError
 import getpass
 
 class pyjenkins:
+    login_url = 'https://ci-comp.tasktop.com/me/my-views/view/All/'
     trigger_url_start = 'https://ci-comp.tasktop.com/job/'
     trigger_url_end = '/build?delay=0sec'
 
@@ -14,6 +16,18 @@ class pyjenkins:
         self.end = pyjenkins.trigger_url_end
         self.username = username
         self.password = password
+
+    def login(self):
+        return self._check_login()
+
+    def _check_login(self):
+        r = requests.get(pyjenkins.login_url, auth=HTTPBasicAuth(self.username, self.password))
+        try:
+            r.raise_for_status()
+        except HTTPError as e:
+            print e
+            return False
+        return True
 
     def _url_builder(self, connector_job_name):
         if 'connector-' in connector_job_name:
@@ -27,12 +41,15 @@ class pyjenkins:
         #full_url = self.start + connector_job_name + self.end
         print full_url
         resp = requests.post(full_url, auth=HTTPBasicAuth(self.username, self.password))
-        return resp
+        resp.raise_for_status()
+        return True
 
 if __name__ == '__main__':
     username = raw_input('Username: ')
     password = getpass.getpass('Password: ')
-    connector_name = raw_input('Connector job name to trigger: ')
 
     jenkins = pyjenkins(username, password)
-    print jenkins.trigger(connector_name)
+
+    if jenkins.login():
+        connector_name = raw_input('Connector job name to trigger: ')
+        print jenkins.trigger(connector_name)
--- pyjenkinstest.py
+++ pyjenkinstest.py
@@ -1,3 +1,5 @@
+'''Test pyJenkins module'''
+
 import unittest
 from pyjenkins import pyjenkins
 
@@ -11,6 +13,20 @@ class PyJenkinsTest(unittest.TestCase):
         PJ = pyjenkins('a','b')
         url = PJ._url_builder('irise')
         self.assertEqual(url, 'https://ci-comp.tasktop.com/job/connector-irise/build?delay=0sec')
+
+    def test_url_building_connector_leankit(self):
+        PJ = pyjenkins('a','b')
+        url = PJ._url_builder('connector-leankit')
+        self.assertEqual(url, 'https://ci-comp.tasktop.com/job/connector-leankit/build?delay=0sec')
+
+    def test_url_building_leankit(self):
+        PJ = pyjenkins('a','b')
+        url = PJ._url_builder('leankit')
+        self.assertEqual(url, 'https://ci-comp.tasktop.com/job/connector-leankit/build?delay=0sec')
+
+    def test_login_fail(self):
+        PJ = pyjenkins('login-fail-test','b')
+        self.assertFalse(PJ.login())
 
 if __name__ == '__main__':
     unittest.main()
